187fed7c01e98b45df1e1c9a6d5f488357593b67
gavinfitch <gavin.fitch@gmail.com>
2021-12-18 11:16:26 -0800

    login working with username or email

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Router, useHistory } from 'react-router-dom';
 import { login } from '../../store/session';
 
 const LoginForm = () => {
@@ -9,6 +9,7 @@ const LoginForm = () => {
   const [password, setPassword] = useState('');
   const user = useSelector(state => state.sessionReducer.user);
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const onLogin = async (e) => {
     e.preventDefault();
@@ -31,34 +32,41 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+    <div className="modal_background">
+      <form className="modal_container" onSubmit={onLogin}>
+        <div className="form_logo">
+          <div className="formLogo_circle"><i id="formLogo_headphones" class="fas fa-headphones"></i></div>Jamify
+        </div>
+        <div>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+        <div className="formInput_wrapper">
+          <input
+            className="form_inputField_song"
+            name='email'
+            type='text'
+            placeholder='Email or username'
+            value={email}
+            onChange={updateEmail}
+          />
+        </div>
+        <div className="formInput_wrapper">
+          <input
+            className="form_inputField_song"
+            name='password'
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={updatePassword}
+          />
+        </div>
+        <div className="dividerLine"></div>
+        <button className="form_submitButton" type='submit'>Login</button>
+        <div>Don't have an account? <span onClick={() => history.push('/sign-up')}>SIGNUP</span></div>
+      </form>
+    </div>
   );
 };
 
